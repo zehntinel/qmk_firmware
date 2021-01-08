adf759207c0e031cfab0e4f02ef358e0f392e7d8
Adrian Galicia <adgaben@gmail.com>
2021-01-08 00:57:05 -0600

    Add linux layer

--- keyboards/lily58/keymaps/tapdance/keymap.c
+++ keyboards/lily58/keymaps/tapdance/keymap.c
@@ -12,6 +12,7 @@ extern uint8_t is_master;
 
 enum layer_number {
   _QWERTY = 0,
+  _LINUX,
   _LOWER,
   _RAISE,
   _ADJUST,
@@ -86,6 +87,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
                         MO(_LOWER), KC_LGUI, KC_LALT, KC_SPC, KC_ENT, KC_BSPC, KC_RGUI, MO(_RAISE) \
 ),
+
+[_LINUX] = LAYOUT( \
+  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV, \
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    TD(CT_UNDERSCORE), \
+  KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    TD(CT_CLN), KC_QUOT, \
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
+                        MO(_LOWER), KC_LCTRL, KC_LALT, KC_SPC, KC_ENT, KC_BSPC, KC_RGUI, MO(_RAISE) \
+),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.ß
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
@@ -123,7 +132,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_RAISE] = LAYOUT( \
-  _______, _______, _______, LCTL(KC_LEFT), LCTL(KC_UP), LCTL(KC_RGHT),              _______,_______ ,_______ , LGUI(LALT(KC_LEFT)), LGUI(LALT(KC_RGHT)), LGUI(LALT(KC_C)), \
+  TG(_LINUX), TG(_QWERTY), _______, LCTL(KC_LEFT), LCTL(KC_UP), LCTL(KC_RGHT),              _______,_______ ,_______ , LGUI(LALT(KC_LEFT)), LGUI(LALT(KC_RGHT)), LGUI(LALT(KC_C)), \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
   KC_F1,  KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,                       XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, \
   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   _______, _______,  KC_PLUS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
@@ -149,7 +158,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
                              _______, _______, _______, _______, _______,  _______, _______, _______ \
-  )
+  ),
 };
 
 // Setting ADJUST layer RGB back to default
